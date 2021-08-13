b34bdb5081b0c8c965a09615116634ff905a1f46
zuozishi <wwb123123@outlook.com>
2021-08-14 01:45:06 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using NPSMLib;
+using System;
+using System.Text.Json;
+
+var sessionManager = new NowPlayingSessionManager();
+var session = sessionManager.CurrentSession;
+var mediaSource = session?.ActivateMediaPlaybackDataSource();
+
+if (session is null || mediaSource is null)
+{
+    Console.Write("{}");
+    Environment.Exit(-1);
+}
+
+var obj = new
+{
+    Session = session,
+    Playback = mediaSource.GetMediaPlaybackInfo(),
+    MediaInfo = mediaSource.GetMediaObjectInfo(),
+    Timeline = mediaSource.GetMediaTimelineProperties(),
+};
+
+string json = JsonSerializer.Serialize(obj, new JsonSerializerOptions
+{
+    WriteIndented = false
+});
+Console.Write(json);
